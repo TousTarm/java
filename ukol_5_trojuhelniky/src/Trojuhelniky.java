0e4836de34a8e4d9e2d8f40ba9c28037a2685a25
TousTarm <tomas.rindt@gmail.com>
2025-04-25 15:19:13 +0200

    Trojuhelnik_ukol + bonus

--- ukol_5_trojuhelniky/src/Trojuhelniky.java
+++ ukol_5_trojuhelniky/src/Trojuhelniky.java
@@ -1,37 +1,19 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Trojuhelniky {
 
-    private ArrayList<Varianta> varianty;   // pole obsahující jednotlivé varianty
-    private CteniZKonsole vstup;            // třída pro vstup z konzole
+    private ArrayList<TypTrojuhelnika> varianty; // List of triangle types
+    private CteniZKonsole vstup;                // Console input handler
 
     public Trojuhelniky() {
-        varianty = new ArrayList<Varianta>();
-        varianty.add(new Varianta(TypTrojuhelnika.PRAVOUHLY_A_B,
-                "Right-angled triangle (sides A and B)",
-                "side A length", "side B length", null));
-        varianty.add(new Varianta(TypTrojuhelnika.PRAVOUHLY_A_C,
-                "Right-angled triangle (side A and hypotenuse C)",
-                "side A length", "hypotenuse C length", null));
-        varianty.add(new Varianta(TypTrojuhelnika.ROVNOSTRANNY,
-                "Equilateral triangle",
-                "side length", null, null));
-        varianty.add(new Varianta(TypTrojuhelnika.OBECNY_ABC,
-                "General triangle (three sides)",
-                "side A length", "side B length", "side C length"));
-        varianty.add(new Varianta(TypTrojuhelnika.OBECNY_AB_ALFA,
-                "General triangle (two sides and included angle)",
-                "side A length", "side B length", "angle α (degrees)"));
-        varianty.add(new Varianta(TypTrojuhelnika.OBECNY_ALFA_BETA_A,
-                "General triangle (two angles and side)",
-                "angle α (degrees)", "angle β (degrees)", "side A length"));
-
+        varianty = new ArrayList<>(Arrays.asList(TypTrojuhelnika.values())); // Load all triangle types
         vstup = new CteniZKonsole();
     }
 
     private void zobrazNabidku() {
         for (int i = 0; i < varianty.size(); i++) {
-            System.out.printf("%2d %s%n", i+1, varianty.get(i).getPopis());
+            System.out.printf("%2d %s%n", i + 1, varianty.get(i).getDescription());
         }
         System.out.println(" 0 Konec");
     }
@@ -43,11 +25,13 @@ public class Trojuhelniky {
             System.out.println("Invalid choice");
             return;
         }
-        Varianta var = varianty.get(index);
-        String dotaz1=var.getDotaz1();
-        String dotaz2=var.getDotaz2();
-        String dotaz3=var.getDotaz3();
-        double [] parametry = new double[var.getPocetParametru()];
+
+        TypTrojuhelnika typ = varianty.get(index);
+        String dotaz1 = typ.getQuestion1();
+        String dotaz2 = typ.getQuestion2();
+        String dotaz3 = typ.getQuestion3();
+        double[] parametry = new double[typ.getParameterCount()];
+
         if (dotaz1 != null) {
             parametry[0] = vstup.getDouble(dotaz1);
         }
@@ -57,20 +41,20 @@ public class Trojuhelniky {
         if (dotaz3 != null) {
             parametry[2] = vstup.getDouble(dotaz3);
         }
-        Trojuhelnik troj = Trojuhelnik.getTrojuhelnik(var.getTyp(), parametry);
+
+        Trojuhelnik troj = Trojuhelnik.getTrojuhelnik(typ, parametry);
         if (troj == null) {
             System.out.println("!!! metoda getTrojuhelnik nevratila pro zadane parametry trojuhelnik !!!!");
-            System.out.println("\t typ: " + var.getTyp());
-            for (int i=0; i < parametry.length; i++) {
-                System.out.printf("\t parametry[%d] : %f%n",i,parametry[i]);
-            }   
-        }
-        else {
+            System.out.println("\t typ: " + typ);
+            for (int i = 0; i < parametry.length; i++) {
+                System.out.printf("\t parametry[%d] : %f%n", i, parametry[i]);
+            }
+        } else {
             System.out.println("=== parametry trojuhelniku ===");
-            System.out.printf("  strany: a=%14f  b=%14f  c=%14f%n",troj.getStranaA(),troj.getStranaB(),troj.getStranaC());
-            System.out.printf("  uhly:   alfa=%11f  beta=%11f  gama=%11f%n",troj.getAlfa(),troj.getBeta(),troj.getGama());
-            System.out.printf("  obvod:  %f%n",troj.obvod());
-            System.out.printf("  obsah:  %f%n%n",troj.obsah());
+            System.out.printf("  strany: a=%14f  b=%14f  c=%14f%n", troj.getStranaA(), troj.getStranaB(), troj.getStranaC());
+            System.out.printf("  uhly:   alfa=%11f  beta=%11f  gama=%11f%n", troj.getAlfa(), troj.getBeta(), troj.getGama());
+            System.out.printf("  obvod:  %f%n", troj.obvod());
+            System.out.printf("  obsah:  %f%n%n", troj.obsah());
         }
     }
 
@@ -83,16 +67,15 @@ public class Trojuhelniky {
         do {
             zobrazNabidku();
             volba = nactiVolbu();
-            if (volba != 0) {  // Changed from -1 to 0
+            if (volba != 0) {
                 zobrazVysledky(volba);
             }
-        } while (volba != 0);  // Changed from -1 to 0
+        } while (volba != 0);
         System.out.println("Konec programu");
     }
-    
-    public static void main (String [] args) {
+
+    public static void main(String[] args) {
         Trojuhelniky troj = new Trojuhelniky();
         troj.zakladniCyklus();
     }
-            
 }
--- ukol_5_trojuhelniky/src/TypTrojuhelnika.java
+++ ukol_5_trojuhelniky/src/TypTrojuhelnika.java
@@ -1,26 +1,48 @@
-
-/**
- * Vyctovy typ TypTrojuhelnika ... představuje jednotlivé typy trojúhelníků,
- * se kterými umí pracovat třídy aplikace Trojuhelniky.
- *
- * @author     Jarmila Pavlíčková
- * @version    1.0, duben 2005
- */
 public enum TypTrojuhelnika {
-    PRAVOUHLY_A_B("Right-angled (a,b)"),
-    PRAVOUHLY_A_C("Right-angled (a,c)"),
-    ROVNOSTRANNY("Equilateral"),
-    OBECNY_ABC("General (a,b,c)"),
-    OBECNY_AB_ALFA("General (a,b,α)"),
-    OBECNY_ALFA_BETA_A("General (α,β,a)");
+    PRAVOUHLY_A_B("Right-angled (a,b)", "Enter side a:", "Enter side b:", null),
+    PRAVOUHLY_A_C("Right-angled (a,c)", "Enter side a:", "Enter hypotenuse c:", null),
+    ROVNOSTRANNY("Equilateral", "Enter side a:", null, null),
+    OBECNY_ABC("General (a,b,c)", "Enter side a:", "Enter side b:", "Enter side c:"),
+    OBECNY_AB_ALFA("General (a,b,α)", "Enter side a:", "Enter side b:", "Enter angle α (in degrees):"),
+    OBECNY_ALFA_BETA_A("General (α,β,a)", "Enter angle α (in degrees):", "Enter angle β (in degrees):", "Enter side a:");
 
     private final String description;
+    private final String question1;
+    private final String question2;
+    private final String question3;
 
-    TypTrojuhelnika(String description) {
+    TypTrojuhelnika(String description, String question1, String question2, String question3) {
         this.description = description;
+        this.question1 = question1;
+        this.question2 = question2;
+        this.question3 = question3;
     }
 
     public String getDescription() {
         return description;
+    }
+
+    public String getQuestion1() {
+        return question1;
+    }
+
+    public String getQuestion2() {
+        return question2;
+    }
+
+    public String getQuestion3() {
+        return question3;
+    }
+
+    public int getParameterCount() {
+        if (question3 != null) {
+            return 3;
+        } else if (question2 != null) {
+            return 2;
+        } else if (question1 != null) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
